c5ea1e270ab0f09c27f04012a0bcb64af942259b
prasanna.000019@gmail.com <prasanna.000019@gmail.com>
2024-07-20 19:12:01 +0530

    second-commit

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 import express from "express"
+import path from "path"
 import dotenv from "dotenv"
 import authRoutes from './routes/authRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import { v2 as cloudinary} from "cloudinary"
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
-import Post from "./models/postModel.js"
 import NotificationRoutes from './routes/NotificationRoute.js'
 import postRoutes from "./routes/postRoutes.js"
 dotenv.config();
@@ -16,7 +16,8 @@ cloudinary.config({
 })
 
 const app=express();
-const PORT=process.env.PORT ||  8000;
+const PORT=process.env.PORT ||  5000;
+const __dirname=path.resolve()
 app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
@@ -25,6 +26,13 @@ app.use('/api/auth',authRoutes);
 app.use('/api/users',userRoutes);
 app.use('/api/posts',postRoutes);
 app.use('/api/notifications',NotificationRoutes);
+if(process.env.NODE_ENV==="production"){
+    console.log("hello")
+    app.use(express.static(path.join(__dirname,"/frontend/dist")))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+    })
+}
 app.listen(PORT,()=>{
     console.log(`${PORT}`);
     connectDB();
